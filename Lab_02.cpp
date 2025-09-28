f376304ea18bbe9d1b6aaea1ee728f7f16226bc9
PavloTeplyi <teplui20@gmail.com>
2025-09-28 14:43:37 +0300

    commit 7

--- Lab_02.cpp
+++ Lab_02.cpp
@@ -7,15 +7,15 @@ int main() {
 	double z1;
 	double z2;
 
-	//cout << "Enter number ";
+	cout << "Enter number ";
 	cin >> a;
 
 	z1 = (sin(a) + sin(a * 5) - sin(a * 3)) / (cos(a) - cos(a * 3) + cos(a * 5));
 	
 	z2 = tan(3 * a);
 	
-	//cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	return 0;
 	
